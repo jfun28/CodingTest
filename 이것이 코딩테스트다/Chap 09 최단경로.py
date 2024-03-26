102f4e2cb9eaf84c0da4014cba53c0a8c2d3984d
jaemin <jmjung1997@gmail.com>
2024-03-26 22:46:02 +0900

    실전문제 전보

--- 이것이 코딩테스트다/Chap 09 최단경로.py
+++ 이것이 코딩테스트다/Chap 09 최단경로.py
@@ -178,20 +178,100 @@
 #     print()
 
 
-# 9-4.py 미래 도시
+# # 9-4.py 미래 도시
 
-INF=int(1e9)
+# INF=int(1e9)
 
-node,edge=map(int, input().split())
+# node,edge=map(int, input().split())
 
-graph=[[INF]*(node+1)for _ in range(n+1)]
+# graph=[[INF]*(node+1)for _ in range(n+1)]
 
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a==b:
-            graph[a][b]=0
+# for a in range(1,n+1):
+#     for b in range(1,n+1):
+#         if a==b:
+#             graph[a][b]=0
 
-# 각 간선에 대한 정보를 입력 받는다.
+# # 각 간선에 대한 정보를 입력 받는다.
+
+# for _ in range(edge):
+#     # 서로 연결되어 있으면 1
+#     n,m=map(int,input().split())
+#     graph[n][m]=1
+#     graph[m][n]=1
+
+# # 거쳐 갈 노드 X와 최종 목적지 노드 K를 입력 받기
+# x,k=map(int, input().split())
+
+# # 점화식에 따라 플로이드 워셜 알고리즘을 수핼
+
+
+
+# 9-5.py 실전문제 전보
+
+import heapq
+import sys
+input=sys.stdin.readline
+
+INF=int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수, 시작노드를 입력 받기
+n,m,start=map(int,input().split())
+
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 만들기
+graph=[[] for i in range(n+1)]
+
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance=[INF]*(n+1)
+
+# 모든 간선 정보를 입력받기
+
+for _ in range(m):
+    x,y,z=map(int,input().split())
+    graph[x].append((y,z))
+
+def dijkstra(start):
+    q=[]
+    heapq.heappush(q,(0,start))
+    distance[start]=0
+    
+    while q:
+        # 가장 최단 거리가 짧은 거리를 가지고 있는 노드 꺼내기
+        dist, node=heapq.heappop(q)
+        if distance[node]<dist:
+            continue
+
+        # 현재 노드와 연결된 다른 인접노드 찾기
+        for i in graph[node]:
+            cost= dist+i[1]
+            if cost<distance[i[0]]:
+                distance[i[0]]=cost
+                heapq.heappush(q,(cost,i[0]))
+
+
+dijkstra(start)
+
+# 도달할 수 있는 노드의 개수
+count=0
+
+# 도달할 수 있는 노드 중에서,가장 멀리 있는 노드와의 최단거리
+max_distance=0
+
+for d in distance:
+    # 도달할 수 있는 노드인 경우에만 카운트를 한다. 
+    if d!=INF:
+        count+=1
+        max_distance=max(max_distance,d)
+
+# 시작노드는 제외해야 하므오 count-1을 출력한다.
+print(count-1,max_distance)
+
+
+
+
+
+
+
+
 
 
 
