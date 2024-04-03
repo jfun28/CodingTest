90364fdc3823051934dc863fd61d4b199686e355
jaemin <jmjung1997@gmail.com>
2024-04-04 00:12:54 +0900

    03 문자열 뒤집기 완성

--- 이것이 코딩테스트다/Chap 11 그리디문제.py
+++ 이것이 코딩테스트다/Chap 11 그리디문제.py
@@ -34,5 +34,24 @@
 
 
 # 3번 문자열 뒤집기
+# 동시 당발적으로 전부 0으로 바꿀때랑 전부 1로 바꿀때로 count 해줘야한다. 
 
-n=int(input())
+data=input()
+count0=0 # 전부 0으로 바꾸는 경우
+count1=0 # 전부 1로 바꾸는 경우
+
+# 첫 번째 원소에 대해서 처리
+
+if data[0]=='1':
+    count0+=1
+else:
+    count1+=1
+
+for i in range(len(data)-1):
+    if data[i]!=data[i+1]:
+        if data[i+1]=='1':
+            count0+=1
+        else:
+            count1+=1
+
+print(min(count0,count1))
