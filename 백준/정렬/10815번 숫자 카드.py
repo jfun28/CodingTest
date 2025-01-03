e1fbeda3dd4d7601595d0b43246e3dc8d6ac2c11
jaemin <jmjung1997@gmail.com>
2025-01-03 23:01:47 +0900

    6-11.py

---
+++ 백준/정렬/10815번 숫자 카드.py
@@ -0,0 +1,19 @@
+from collections import deque
+n=int(input())
+
+n_list=list(map(int,input().split()))
+
+m=int(input())
+
+m_list=list(map(int,input().split()))
+
+_dict={}
+for i in range(len(n_list)):
+    _dict[n_list[i]]=0
+print("check")
+
+for j in range(m):
+    if m_list[j] not in _dict:
+        print(0,end=' ')
+    else:
+        print(1,end=' ')
