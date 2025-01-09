c3f3bff2d309c1ce775448111426382df28b1811
jaemin <jmjung1997@gmail.com>
2025-01-09 23:36:32 +0900

    14426번 접두사 찾기.py 성공

--- 백준/이진 탐색/14426번 접두사 찾기.py
+++ 백준/이진 탐색/14426번 접두사 찾기.py
@@ -9,8 +9,26 @@ for _ in range(n):
 for _ in range(m):
     m_list.append(input())
 
-
+n_list.sort()
 
 def binary_search(array,target,start,end):
     while start<=end:
-        
+        mid=(start+end)//2
+        if array[mid][:len(target)]==target:
+            return mid
+        elif target<=array[mid]:
+            end=mid-1
+        else:
+            start=mid+1
+    return None
+    
+
+count=0
+
+for word in m_list:
+    if binary_search(n_list,word,0,n-1)!=None:
+        count+=1
+    else:
+        pass
+
+print(count)
