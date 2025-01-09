69270afc68668ea1ee6eb481bd919e3d1b6c394a
jaemin <jmjung1997@gmail.com>
2025-01-09 23:08:30 +0900

    14426번 접두사 2day

--- 백준/이진 탐색/14426번 접두사 찾기.py
+++ 백준/이진 탐색/14426번 접두사 찾기.py
@@ -1,8 +1,16 @@
 n,m=map(int,input().split())
 
 n_list=[]
+m_list=[]
 
 for _ in range(n):
     n_list.append(input())
 
-print(n_list)
+for _ in range(m):
+    m_list.append(input())
+
+
+
+def binary_search(array,target,start,end):
+    while start<=end:
+        
