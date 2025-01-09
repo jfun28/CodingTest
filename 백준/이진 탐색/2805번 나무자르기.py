d121bde06a4939df58394ba202da93f863553b83
jaemin <jmjung1997@gmail.com>
2025-01-10 00:16:36 +0900

    2805번 나무자르기 시간부족

---
+++ 백준/이진 탐색/2805번 나무자르기.py
@@ -0,0 +1,24 @@
+n,m=map(int,input().split())
+
+tree=list(map(int,input().split()))
+
+start=0
+end=max(tree)
+result=0
+while(start<=end):
+    total=0
+    mid=(start+end)//2
+    for x in tree:
+        if x>mid:
+            total+=(x-mid)
+
+    # 나무의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
+    if total<m:
+        end=end-1
+    else: # 나무의 양이 넘치는 경우 조금 덜 자르기(오른쪽 부분 탐색)
+        result=mid
+        start=start+1
+
+
+
+print(result)
