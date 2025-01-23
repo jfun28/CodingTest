f6a8bff4d15d8019e77499ce8984c79c7a4ba948
jaemin <jmjung1997@gmail.com>
2025-01-24 00:31:01 +0900

    1965번 상자 놓기 답안지 봄

--- 백준/다이나믹/1965번 상자넣기.py
+++ 백준/다이나믹/1965번 상자넣기.py
@@ -2,5 +2,11 @@ n=int(input())
 
 box=list(map(int,input().split()))
 
-dp=[0]*n
-print(dp)
+dp=[1]*n
+
+for i in range(1,n):
+    for j in range(i):
+        if box[i]>box[j]:
+            dp[i]=max(dp[i],dp[j]+1)
+
+print(max(dp))
