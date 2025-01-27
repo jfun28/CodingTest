b3a539f94ed301b834ae06001f446afdb6b4254c
jaemin <jmjung1997@gmail.com>
2025-01-28 01:20:26 +0900

    18353번 병사 미완성

---
+++ 백준/다이나믹/18353번 병사 배치하기.py
@@ -0,0 +1,9 @@
+n=int(input())
+dp=[1]*n
+soldier=list(map(int,input().split()))
+
+for i in range(1,n):
+    for j in range(0,i):
+        if soldier[j]>soldier[i]:
+            dp[i]=max(dp[i],dp[j]+1)
+print(max(dp))
