29dc49504cc8bc5df238305ac4bfcab75e906b40
jaemin <jmjung1997@gmail.com>
2025-02-05 00:24:10 +0900

    2458번 키순서 자료형으로 구현

--- 백준/최단 경로/2458번 키순서.py
+++ 백준/최단 경로/2458번 키순서.py
@@ -1,17 +1,11 @@
 import sys
-INF=sys.maxsize
+# INF=sys.maxsize
 # 노드의 개수 및 간선의 개수를 입력받기
+input=sys.stdin.readline
 n,m=map(int,input().split())
 
-graph=[[INF]*(n+1) for _ in range(n+1)]
+graph=[[0]*(n+1) for _ in range(n+1)]
 
-
-
-# 자기자신으로 가는 비용은 0으로 초기화
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a==b:
-            graph[a][b]=0
 
 # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
 for _ in range(m):
@@ -22,17 +16,19 @@ for _ in range(m):
 # 점화식에 따라 플로이드 워셜 알고리즘을 수행
 for k in range(1,n+1):
     for a in range(1,n+1):
-        for b in range(1,n+1):
-            graph[a][b]=min(graph[a][b],graph[a][k]+graph[k][b])
+        if graph[a][k]:  # a에서 k로 가는 경로가 있을 때만 확인
+            for b in range(1, n+1):
+                if graph[k][b]:  # k에서 b로 가는 경로가 있을 때만 확인
+                    graph[a][b] = 1
 
-# 수행된 결과를 출력
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        # 도달할 수 없는 경우, 무한이라고 출력
-        if graph[a][b]==INF:
-            print("INFINITY", end=" ")
-        # 도달할 수 있는 경우 거리를 출력 
-        else:
-            print(graph[a][b],end=" ")
 
-    print()
+result = 0
+for i in range(1, n+1):
+    sum = 0
+    for j in range(1, n+1):
+        sum += graph[i][j] + graph[j][i] # 어떤 노드로 기준으로 나아가는것과 들어오는 경우 모두 다 체크크
+    if sum == n-1:
+        result += 1
+print(result)
+
+# 자기 순서를 알고있는 학생 수를 출력하는 것
