cce8d9c065e0dc1c1c06fef00c2100a1e92eae1e
jaemin <jmjung1997@gmail.com>
2025-02-06 01:01:03 +0900

    2660번 회장 뽑기 파이널 정답

--- 백준/최단 경로/2660번 회장뽑기.py
+++ 백준/최단 경로/2660번 회장뽑기.py
@@ -1,5 +1,5 @@
 import sys
-INF=sys.maxsize
+INF=50
 n=int(input())
 
 graph=[[INF]*(n+1) for _ in range(n+1)]
@@ -18,7 +18,6 @@ for k in range(1,n+1):
             if graph[a][b] > graph[a][k]+graph[k][b]:
                 graph[a][b]=graph[a][k]+graph[k][b]
 
-print(graph)
 
 cm, cm_score = [], INF
 for i in range(1, n+1):
@@ -26,7 +25,7 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         score = max(score, graph[i][j])
     if score < cm_score:
-        cm = []
+        cm = [i] # 완전 작은값을 가지고 있는 리스트가 있을 수 있으니(모두 다 1인) 이 경우는 현재 i가 걍 회장이다다
         cm_score = score
     elif score == cm_score:
         cm.append(i)
