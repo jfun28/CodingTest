ffe5e137fa8fe8213133751b44fc8d6177640272
my_github_jmjung1997 <my_github_jmjung1997@naver.com>
2025-02-08 22:24:53 +0900

    중간저장

--- 백준/DFS,BFS/1012번 유기농 배추.py
+++ 백준/DFS,BFS/1012번 유기농 배추.py
@@ -1,7 +1,6 @@
 import sys
 input=sys.stdin.readline
 
-t=int(input())
 
 def dfs(x,y):
     dx=[1,-1,0,0]
@@ -12,18 +11,28 @@ def dfs(x,y):
         nx=x+dx[i]
         ny=y+dy[i]
         
-        if (0<=nx<n) or (0<=ny<m):
-            if graph[nx][ny]==1:
-                graph[nx][ny]=-1
+        if (0<=nx<m) or (0<=ny<n):
+            if graph[ny][nx]==1:
+                graph[ny][nx]=-1
                 dfs(nx,ny)
+
+
+t=int(input())
                 
 for _ in range(t):
-    n,m,v=map(int,input().split())
+    m,n,v=map(int,input().split())
 
     graph=[[0]*m for _ in range(n)]
-
+    result=0
     for _ in range(v):
         a,b=map(int,input().split())
         graph[a][b]=graph[b][a]=1
-        
-    visited=[[False]*m for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(m):
+            if graph[j][i]==1: # x축 y축 헷갈리지 말것
+                dfs(i,j)
+                result+=1
+                
+# 출력
+print(result)    
