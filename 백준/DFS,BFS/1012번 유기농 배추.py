4f53ba60e456665145ae68361408a302a78e2137
my_github_jmjung1997 <my_github_jmjung1997@naver.com>
2025-02-08 22:24:53 +0900

    1012번 유기농 배추

---
+++ 백준/DFS,BFS/1012번 유기농 배추.py
@@ -0,0 +1,29 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+
+def dfs(x,y):
+    dx=[1,-1,0,0]
+    dy=[0,0,1,-1]
+    
+    # 상하좌우 확인
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        
+        if (0<=nx<n) or (0<=ny<m):
+            if graph[nx][ny]==1:
+                graph[nx][ny]=-1
+                dfs(nx,ny)
+                
+for _ in range(t):
+    n,m,v=map(int,input().split())
+
+    graph=[[0]*m for _ in range(n)]
+
+    for _ in range(v):
+        a,b=map(int,input().split())
+        graph[a][b]=graph[b][a]=1
+        
+    visited=[[False]*m for _ in range(n)]
--- 프로그래머스/양궁대회.py
+++ 프로그래머스/양궁대회.py
@@ -5,7 +5,7 @@ def is_winner_with_gap(score, info):
     a = 0  # 어피치 점수
     b = 0  # 라이언 점수
     
-    for i in range(len(info)):
+    for i in range(len(info)): 
         if info[i] > 0 or score[i] > 0:
             if info[i] >= score[i]:
                 a += (10-i)
@@ -15,8 +15,8 @@ def is_winner_with_gap(score, info):
 
 def dfs(L, cnt, score, info, n):
     global max_gap, answer
-    if L == 11 or cnt == 0: # 
-        is_winner, gap = is_winner_with_gap(score, info)
+    if L == 11 or cnt == 0: # 화살을 다 쏜 경우(모든 과녁을 다 검토하였거나 남은 화살의 개수 0개이거나) 
+        is_winner, gap = is_winner_with_gap(score, info) 
         if is_winner:
             if cnt >= 0:  # 화살이 남은 경우
                 score[10] = cnt  # 0점에 쏴도 이김
@@ -36,13 +36,13 @@ def dfs(L, cnt, score, info, n):
                 
         return
     
-    # k점을 어피치보다 많이 맞추거나 아예 안맞추거나
-    if cnt > info[L]:
-        score[L] = info[L]+1
-        dfs(L+1, cnt-(info[L]+1), score, info, n)
-        score[L] = 0
+    # k점을 어피치보다 많이 맞추거나 아예 안맞추거나 -> 점수를 더 높게 받거나 아예 포기하거나 후일을 노리거나
+    if cnt > info[L]: # 현재 남은 화살이 어피치가 해당 점수에 쏜 화살 보다 많은지 확인
+        score[L] = info[L]+1 # 어피치보다 1발 더 많이 쏘기로 결정
+        dfs(L+1, cnt-(info[L]+1), score, info, n) # 다음 점수로 넘어감(L+1), 사용한 화살만큼 cnt에서 빼줌,현재까지의 점수 상태를 가지고 계속 탐색
+        score[L] = 0 # 백트래킹 
         
-    dfs(L+1, cnt, score, info, n)
+    dfs(L+1, cnt, score, info, n) # 백트래킹으로 시도하는것 
 
 def solution(n, info):
     global max_gap, answer
