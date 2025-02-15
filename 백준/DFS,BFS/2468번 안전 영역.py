5f316f5c596cc45f111f0a7936255e73de8e9472
jaemin <jmjung1997@gmail.com>
2025-02-15 14:33:09 +0900

    2468번 안전 영역 init3

--- 백준/DFS,BFS/2468번 안전 영역.py
+++ 백준/DFS,BFS/2468번 안전 영역.py
@@ -2,10 +2,33 @@ import sys
 input=sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
+
+def dfs(x,y,h): # 받아야 되는것 x,y 위치와 h 높이 임계값
+    visted[x][y]=1
+
+    f
+
+
+
+
+
+
+
 n=int(input)
 graph=[]
+maxNum=0
+for i in range(n):
+    graph.append(list(map(int, input().split()))) # 여기서는 그래프를 입력받고
 
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+# 2차원 리스트 최솟값, 최댓값
+min_h=min(map(min,graph))
+max_h=max(map(max,graph))
 
-def dfs():
+safe_area_num=0
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+for h in range(min_h-1,max_h):
+    
+
