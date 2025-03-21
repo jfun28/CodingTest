2c74f0bce5e355b392b6b16334fa293e7898fd03
my_github_jmjung1997 <jmjung1997@naver.com>
2025-03-21 12:54:01 +0900

    프로그래머스 서버 증설 횟수

---
+++ 프로그래머스/서버 증설 횟수.py
@@ -0,0 +1,25 @@
+def solution(players, m, k):
+    server=[0]*len(players)
+    cnt=0
+    
+    for i in range(len(players)):
+        remain=players[i]-(server[i]*m)
+        if remain >=m:
+            end=min(i+k,len(players))
+            for j in range(i,end):
+                server[j]+=remain//m
+            cnt+=remain//m
+    return cnt
+    
+
+
+
+players=list(map(int,input().split()))
+m=int(input())
+k=int(input())
+
+
+
+
+print(solution(players, m, k))
+
