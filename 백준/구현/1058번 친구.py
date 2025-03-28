ef49be34bbf4051b9fc0b4a25b5efc95d6c56b8b
jaemin <jmjung1997@gmail.com>
2025-03-28 21:48:56 +0900

    1036번 킹 init

--- 백준/구현/1058번 친구.py
+++ 백준/구현/1058번 친구.py
@@ -5,10 +5,10 @@ for _ in range(n):
     graph.append(list(input().strip()))
 
 friends=[[0]*n for i in range(n)]
-for i in range(n): # 자기 본인
-    for j in range(n):
-        for k in range(n):
-            if k==j: # 본인 자신
+for i in range(n): # 2친구구
+    for j in range(n): # 본인 자신
+        for k in range(n): # 절친친
+            if k==j:
                 continue
 
             if graph[j][k]=='Y' or (graph[j][i]=='Y' and graph[i][k]=='Y'):# 나랑 친구이거나 한다리 건너면 아는 사이
