51a2eb5a072a2e4d6abf1712d23e5f1c52c755e6
jaemin <jmjung1997@gmail.com>
2025-07-06 16:11:13 +0900

    1182번 부분수열의 합

--- 백준/구현/2615번 오목.py
+++ 백준/구현/2615번 오목.py
@@ -1,25 +1,23 @@
 import sys
-input=sys.stdin.readline
-n=19
 
+n=19
+graph=[]
 blacks=[]
 whites=[]
-board=[]
-
 
 for _ in range(n):
-    board.append(list(map(int,input().split())))
+    graph.append(list(map(int,input().split())))
 
 for i in range(n):
     for j in range(n):
-        if board[i][j]==1:
+        if graph[i][j]==1:
             blacks.append((i,j))
-
-        elif board[i][j]==2:
+        elif graph[i][j]==2:
             whites.append((i,j))
 
+
 def search(omoks):
-    dx=[1,1,0,-1] # 대각선, 가로, 세로,반대 대각선
+    dx=[1,1,0,-1]
     dy=[1,0,1,1]
 
     for x,y in omoks:
@@ -31,34 +29,42 @@ def search(omoks):
 
                 if nx<0 or nx>=n or ny<0 or ny>=n:
                     continue
+
                 if (nx,ny) not in omoks:
                     break
+
                 if (nx,ny) in omoks:
                     cnt+=1
 
-            tx=x-dx[i] # 시작점에서 한칸 옆에도 돌이 있는가 
+            tx=x-dx[i]
             ty=y-dy[i]
 
             if (tx,ty) not in omoks and cnt==4:
-                return (x+1,y+1) # 컴퓨터는 0 부터 시작하니깐 실제 좌표 처럼 보이려면 1을 더해줘야함
-    
+                return (x+1,y+1)
+
     return False
 
+  
 
-black_research=search(blacks)
-white_research=search(whites)
+blacks_research=search(blacks)
+whites_research=search(whites)
 
-if black_research:
+
+
+
+if blacks_research:
     print(1)
-    print(*black_research)
+    print(*blacks_research)
 
-elif white_research:
+elif whites_research:
     print(2)
-    print(*white_research)
+    print(*whites_research)
 
 else:
     print(0)
-       
+
+
+
 
 
 
@@ -76,18 +82,12 @@ else:
 
 # import sys
 # input=sys.stdin.readline
-
-# '''
-# - 출력해야하는 제일 왼쪽 좌표를 기준으로 주 대각선, 세로줄, 가로줄, 부 대각선 순으로 하여 4개의 방향을 탐색
-# - 단, 조건으로 6개 연속인 바둑알은 승리가 아니므로, 연속된 조건의 count를 자신을 제외하고 각 방향으로 5번 까지만 진행한 후 연속된 바둑알의
-# 갯수가 4개인 경우만 승리로 간주한다
-# '''
-
 # n=19
 
 # blacks=[]
 # whites=[]
 # board=[]
+
 
 # for _ in range(n):
 #     board.append(list(map(int,input().split())))
@@ -101,8 +101,8 @@ else:
 #             whites.append((i,j))
 
 # def search(omoks):
-#     dx = [1, 1, 0, -1]
-#     dy = [1, 0, 1, 1]
+#     dx=[1,1,0,-1] # 대각선, 가로, 세로,반대 대각선
+#     dy=[1,0,1,1]
 
 #     for x,y in omoks:
 #         for i in range(4):
@@ -111,32 +111,114 @@ else:
 #                 nx=x+dx[i]*k
 #                 ny=y+dy[i]*k
 
-#                 if nx<0 or nx>=n or ny <0 or ny>=n:
+#                 if nx<0 or nx>=n or ny<0 or ny>=n:
 #                     continue
 #                 if (nx,ny) not in omoks:
 #                     break
 #                 if (nx,ny) in omoks:
 #                     cnt+=1
 
-#             tx=x-dx[i] # 시작점의 반대 한칸 옆에도 돌이 있는가 체크
+#             tx=x-dx[i] # 시작점에서 한칸 옆에도 돌이 있는가 
 #             ty=y-dy[i]
 
-#             if (tx,ty) not in omoks and cnt ==4:
-#                 return (x+1,y+1)
-            
+#             if (tx,ty) not in omoks and cnt==4:
+#                 return (x+1,y+1) # 컴퓨터는 0 부터 시작하니깐 실제 좌표 처럼 보이려면 1을 더해줘야함
+    
 #     return False
 
 
-# black_result=search(blacks)
-# white_result=search(whites)
+# black_research=search(blacks)
+# white_research=search(whites)
 
-# if black_result:
+# if black_research:
 #     print(1)
-#     print(*black_result)
+#     print(*black_research)
 
-# elif white_result:
+# elif white_research:
 #     print(2)
-#     print(*white_result)
+#     print(*white_research)
 
 # else:
 #     print(0)
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import sys
+# # input=sys.stdin.readline
+
+# # '''
+# # - 출력해야하는 제일 왼쪽 좌표를 기준으로 주 대각선, 세로줄, 가로줄, 부 대각선 순으로 하여 4개의 방향을 탐색
+# # - 단, 조건으로 6개 연속인 바둑알은 승리가 아니므로, 연속된 조건의 count를 자신을 제외하고 각 방향으로 5번 까지만 진행한 후 연속된 바둑알의
+# # 갯수가 4개인 경우만 승리로 간주한다
+# # '''
+
+# # n=19
+
+# # blacks=[]
+# # whites=[]
+# # board=[]
+
+# # for _ in range(n):
+# #     board.append(list(map(int,input().split())))
+
+# # for i in range(n):
+# #     for j in range(n):
+# #         if board[i][j]==1:
+# #             blacks.append((i,j))
+
+# #         elif board[i][j]==2:
+# #             whites.append((i,j))
+
+# # def search(omoks):
+# #     dx = [1, 1, 0, -1]
+# #     dy = [1, 0, 1, 1]
+
+# #     for x,y in omoks:
+# #         for i in range(4):
+# #             cnt=0
+# #             for k in range(1,6): # 대각선을 다루는 방법
+# #                 nx=x+dx[i]*k
+# #                 ny=y+dy[i]*k
+
+# #                 if nx<0 or nx>=n or ny <0 or ny>=n:
+# #                     continue
+# #                 if (nx,ny) not in omoks:
+# #                     break
+# #                 if (nx,ny) in omoks:
+# #                     cnt+=1
+
+# #             tx=x-dx[i] # 시작점의 반대 한칸 옆에도 돌이 있는가 체크
+# #             ty=y-dy[i]
+
+# #             if (tx,ty) not in omoks and cnt ==4:
+# #                 return (x+1,y+1)
+            
+# #     return False
+
+
+# # black_result=search(blacks)
+# # white_result=search(whites)
+
+# # if black_result:
+# #     print(1)
+# #     print(*black_result)
+
+# # elif white_result:
+# #     print(2)
+# #     print(*white_result)
+
+# # else:
+# #     print(0)
