585f081f5de3b50786efb3a7e540853af94e5693
jaemin <jmjung1997@gmail.com>
2025-07-09 23:10:42 +0900

    12933번 오리

--- 백준/백트래킹/1182번 부분수열의 합.py
+++ 백준/백트래킹/1182번 부분수열의 합.py
@@ -1,27 +1,28 @@
-n,s=map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-array=list(map(int,input().split()))
-cnt=0
-ans=[]
+n, s = map(int, input().split())
+array = list(map(int, input().split()))
+cnt = 0
+answer = []
 
 def dfs(start):
     global cnt
-    if sum(ans)==s and len(ans)>0:
-        cnt+=1
 
-    for i in range(start,n):
-        ans.append(array[i])
-        dfs(i+1)
-        ans.pop()
+    print(f"[CALL] dfs({start}) | current answer = {answer}")
+
+    if sum(answer) == s and len(answer) > 0:
+        cnt += 1
+        print(f"  ➤ 합이 {s}인 부분 수열 발견! -> {answer}")
+
+    for i in range(start, n):
+        answer.append(array[i])
+        print(f"  ├─ append {array[i]} → answer = {answer}, next start = {i+1}")
+        
+        dfs(i + 1)
+
+        popped = answer.pop()
+        print(f"  └─ pop {popped} → answer = {answer} (backtrack to dfs({start}))")
 
 dfs(0)
-print(cnt)
-
-
-
-
-
-    
-
-
-
+print(f"\n총 경우의 수: {cnt}")
