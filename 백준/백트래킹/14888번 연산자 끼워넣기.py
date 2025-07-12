02568dc09bcecf0d11f69cdeb036c591556c8a0e
jaemin <jmjung1997@gmail.com>
2025-07-12 16:10:48 +0900

    14888번 연산자 끼워넣기 복습

--- 백준/백트래킹/14888번 연산자 끼워넣기.py
+++ 백준/백트래킹/14888번 연산자 끼워넣기.py
@@ -1,37 +1,39 @@
 import sys
 input=sys.stdin.readline
 
-
-
 n=int(input())
+num_list=list(map(int,input().split()))
 
-array=list(map(int,input().split()))
+op=list(map(int,input().split()))
+answer=0
+min_ans=int(1e9)
+max_ans=-int(1e9)
 
-op=list(map(int,input().split())) # +, -, *, /
-maximum=-int(1e9)
-minimum=int(1e9)
+def search(depth, answer, plus, minus, multiply,divide):
+    global min_ans,max_ans
 
-def dfs(depth,total,plus,minus,multiply,divide):
-    global maximum,minimum
-    
     if depth==n:
-        minimum=min(total,minimum)
-        maximum=max(total,maximum)
+        max_ans=max(max_ans, answer)
+        min_ans=min(min_ans,answer)
+        return
 
-    if plus>0:
-        dfs(depth+1,total+array[depth],plus-1,minus,multiply,divide)
-    
-    if minus>0:
-        dfs(depth+1,total-array[depth],plus,minus-1,multiply,divide)
+   
+    if plus:
+        search(depth+1, answer+num_list[depth], plus-1, minus, multiply,divide)
 
-    if multiply>0:
-        dfs(depth+1,total*array[depth],plus,minus,multiply-1,divide)
 
-    if divide>0:
-        dfs(depth+1,int(total/array[depth]),plus,minus,multiply,divide-1)
+    elif minus:
+        search(depth+1, answer-num_list[depth], plus, minus-1, multiply,divide)
 
-dfs(1,array[0],op[0],op[1],op[2],op[3])
 
-print(maximum)
-print(minimum)
+    elif multiply:
+        search(depth+1, answer*num_list[depth], plus, minus, multiply-1,divide)
 
+
+    elif divide:
+        search(depth+1, int(answer/num_list[depth]), plus, minus, multiply,divide-1)
+
+search(1, num_list[0], op[0], op[1], op[2],op[3])
+
+print(max_ans)
+print(min_ans)
