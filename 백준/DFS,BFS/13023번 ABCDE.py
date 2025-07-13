87dc55ff4e14850736f759eed7c9ab4459ea494f
jaemin <jmjung1997@gmail.com>
2025-07-13 21:06:10 +0900

    13023번 ABCDE 복습

--- 백준/DFS,BFS/13023번 ABCDE.py
+++ 백준/DFS,BFS/13023번 ABCDE.py
@@ -4,54 +4,37 @@ sys.setrecursionlimit(100000)
 
 n,m=map(int,input().split())
 
-graph=[[]*n for _ in range(n)]
+graph=[[] for _ in range(n)]
 
-count=0
-
-
-arrive = False
+arrive=False
 
 for _ in range(m):
     a,b=map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-
-def dfs(start,depth):
+def dfs_search(start, depth, visited):
     global arrive
     visited[start]=True
-    '''
-    if 문을 통해 어떻게 완전히 연결되었다는 것을 알 수 있을까?
-    아니면 반환의 조건은 무엇인가?
-    '''
+
     if depth==5:
         arrive=True
-        return 
-    
+        return arrive
+
     for i in graph[start]:
         if not visited[i]:
-            dfs(i,depth+1)
-    visited[start]=False
-         
+            dfs_search(i,depth+1,visited)
+    visited[start]=False # 
 
 
 for i in range(n):
     visited=[False]*n
-    dfs(i,1)
+    dfs_search(i,1,visited)
     if arrive:
         break
 
 if arrive:
     print(1)
+
 else:
     print(0)
-
-
-        
-
-
-
-
-
-
-
