ff0610a571f64517d22ae7930fb3a1c144f0ce58
jaemin <jmjung1997@gmail.com>
2025-07-22 23:47:26 +0900

    16918번 봄버맨 init

--- 백준/백트래킹/14888번 연산자 끼워넣기.py
+++ 백준/백트래킹/14888번 연산자 끼워넣기.py
@@ -1,16 +1,45 @@
 import sys
 input=sys.stdin.readline
 
-oper=list(map(int,input().split()))
 
+n=int(input())
 num_list=list(map(int,input().split()))
 
-def dfs(start):
+
+oper=list(map(int,input().split()))
 
 
+min_num=1e9
+max_num=-1e9
+
+sum_num=0
+
+def dfs(depth,sum_num,plus,minus,multiplly,divide):
+    global max_num, min_num
+    
+    if depth==n:
+        min_num=min(min_num,sum_num)
+        max_num=max(max_num,sum_num)
+        return
+
+    if plus>0: # 플러스
+        
+        dfs(depth+1,sum_num+num_list[depth],plus-1,minus,multiplly,divide)
+
+    if minus>0: # 마이너스
+       
+        dfs(depth+1,sum_num-num_list[depth],plus,minus-1,multiplly,divide)
+
+    if multiplly>0: # 곱하기
+        
+        dfs(depth+1,sum_num*num_list[depth],plus,minus,multiplly-1,divide)
+
+    if divide>0: # 나누기
+        
+        dfs(depth+1,int(sum_num/num_list[depth]),plus,minus,multiplly,divide-1)
 
 
+dfs(0,0,oper[0],oper[1],oper[2],oper[3])
 
-    return
-
-dfs(0)
+print(max_num)
+print(min_num)
