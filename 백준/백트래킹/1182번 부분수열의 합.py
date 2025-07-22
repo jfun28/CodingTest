08eb3b311c34343b969fa8c835433c2f7f702008
jaemin <jmjung1997@gmail.com>
2025-07-22 14:10:29 +0900

    14888번 연산자 끼워 넣기 복습

--- 백준/백트래킹/1182번 부분수열의 합.py
+++ 백준/백트래킹/1182번 부분수열의 합.py
@@ -1,28 +1,26 @@
-# import sys
-# input = sys.stdin.readline
+n,s=map(int,input().split())
 
-# n, s = map(int, input().split())
-# array = list(map(int, input().split()))
-# cnt = 0
-# answer = []
+n_list=list(map(int,input().split()))
 
-# def dfs(start):
-#     global cnt
+cnt=0
 
-#     print(f"[CALL] dfs({start}) | current answer = {answer}")
+ans=[]
 
-#     if sum(answer) == s and len(answer) > 0:
-#         cnt += 1
-#         print(f"  ➤ 합이 {s}인 부분 수열 발견! -> {answer}")
+def dfs_search(start):
+    global cnt
+    if sum(ans)==s and len(ans)>0:
+        cnt+=1
+    
 
-#     for i in range(start, n):
-#         answer.append(array[i])
-#         print(f"  ├─ append {array[i]} → answer = {answer}, next start = {i+1}")
-        
-#         dfs(i + 1)
+    for i in range(start,n):
+        ans.append(n_list[i])
+        dfs_search(i+1)
+        ans.pop()
 
-#         popped = answer.pop()
-#         print(f"  └─ pop {popped} → answer = {answer} (backtrack to dfs({start}))")
+    return cnt
 
-# dfs(0)
-# print(f"\n총 경우의 수: {cnt}")
+result=dfs_search(0)
+
+ 
+
+print(result)
--- 백준/백트래킹/14888번 연산자 끼워넣기.py
+++ 백준/백트래킹/14888번 연산자 끼워넣기.py
@@ -1,39 +1,16 @@
 import sys
 input=sys.stdin.readline
 
-n=int(input())
+oper=list(map(int,input().split()))
+
 num_list=list(map(int,input().split()))
 
-op=list(map(int,input().split()))
-answer=0
-min_ans=int(1e9)
-max_ans=-int(1e9)
-
-def search(depth, answer, plus, minus, multiply,divide):
-    global min_ans,max_ans
-
-    if depth==n:
-        max_ans=max(max_ans, answer)
-        min_ans=min(min_ans,answer)
-        return
-
-   
-    if plus:
-        search(depth+1, answer+num_list[depth], plus-1, minus, multiply,divide)
+def dfs(start):
 
 
-    if minus:
-        search(depth+1, answer-num_list[depth], plus, minus-1, multiply,divide)
 
 
-    if multiply:
-        search(depth+1, answer*num_list[depth], plus, minus, multiply-1,divide)
 
+    return
 
-    if divide:
-        search(depth+1, int(answer/num_list[depth]), plus, minus, multiply,divide-1)
-
-search(1, num_list[0], op[0], op[1], op[2],op[3])
-
-print(max_ans)
-print(min_ans)
+dfs(0)
