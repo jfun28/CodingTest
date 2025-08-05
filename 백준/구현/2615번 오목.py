9543c398e93750845841356676e66475c5928f34
jaemin <jmjung1997@gmail.com>
2025-08-06 00:37:59 +0900

    1047번 z

--- 백준/구현/2615번 오목.py
+++ 백준/구현/2615번 오목.py
@@ -1,61 +1,65 @@
-'''
-1. 한번 뒤로 가서 점검해볼 것
-'''
-
-n=19
+import sys
+input=sys.stdin.readline
+k=19
 graph=[]
-whites=[]
-blacks=[]
+whites_blank=[]
+blacks_blank=[]
 
-for _ in range(n):
+for _ in range(k):
     graph.append(list(map(int,input().split())))
 
-for i in range(n):
-    for j in range(n):
+for i in range(k):
+    for j in range(k):
         if graph[i][j]==1:
-            blacks.append((i,j))
+            blacks_blank.append((i,j))
 
         elif graph[i][j]==2:
-            whites.append((i,j))
-        
+            whites_blank.append((i,j))
+
 
 def search(omoks):
-    dx=[1,0,1,-1]
-    dy=[1,1,0,1]
+    dx=[1,1,0,-1]
+    dy=[1,0,1,1]
 
-    for r,c in omoks:
+    for x,y in omoks:
         for i in range(4):
             cnt=0
-            for k in range(1,6):
-                nr=r+dx[i]*k
-                nc=c+dy[i]*k
+            for g in range(1,6): # 
+                nx=x+dx[i]*g
+                ny=y+dy[i]*g
 
-                if nr<0 or nr>=n or nc<0 or nc>=n:
+                if nx<0 or nx>=k or ny<0 or ny>=k:
                     continue
 
-                if (nr,nc) not in omoks:
+                if (nx,ny) not in omoks:
                     break
 
-                if (nr,nc) in omoks:
+                if (nx,ny) in omoks:
                     cnt+=1
+                   
 
-            tr=r-dx[i]
-            tc=c-dy[i]
+            tx=x-dx[i]
+            ty=y-dy[i]
 
-            if (tr,tc) not in omoks and cnt==4:
-                return (r+1,c+1)
-    
+            if (tx,ty) not in omoks and cnt==4:
+                return (x+1,y+1)
+            
     return False
 
 
-white_search=search(whites)
-black_search=search(blacks)
+white_find=search(whites_blank)
+blacks_find=search(blacks_blank)
 
-if white_search:
-    print(2)
-    print(*white_search)
-
-elif black_search:
+if blacks_find:
     print(1)
-    print(*black_search)
+    print(*blacks_find)
+
+
+elif white_find:
+    print(2)
+    print(*white_find)
+
+
+else:
+    print(0)
 
--- 백준/브루투 포스/2529번 부등호.py
+++ 백준/브루투 포스/2529번 부등호.py
@@ -1,34 +1,38 @@
-def get_num(x,y,oper):
+import sys
+input=sys.stdin.readline
+
+k=int(input())
+visited=[False]*10
+ans=[]
+equa=list(input().split())
+
+def check(x,y,oper):
     if oper=='<':
         if x>y:
             return False
     else:
         if x<y:
             return False
-        
     return True
 
-def dfs(idx,num):
+def dfs_find(idx,num):
+
     if idx==k+1:
         ans.append(num)
         return
-    
+
     for i in range(10):
-        if not check[i]:
-            if idx==0 or get_num(num[idx-1],str(i),oper[idx-1]):
-                check[i]=True
-                dfs(idx+1,num+str(i))
-                check[i]=False
-                   
+        if not visited[i]:
+            if idx==0 or check(num[idx-1],str(i),equa[idx-1]):
+                visited[i]=True
+                dfs_find(idx+1,num+str(i))
+                visited[i]=False
 
-k=int(input())
-oper=list(input().split())
-
-check=[False]*10
-ans=[]
-dfs(0,'')
+dfs_find(0,'')
 
 ans.sort()
 
-print(ans[-1])
+print(type(ans[-1]))
 print(ans[0])
+
+
