12a0912cf55d659685c3591de0d11ccd8f406399
jaemin <jmjung1997@gmail.com>
2025-08-14 00:00:58 +0900

    9079번 동전 게임 init

--- 백준/DFS,BFS/9205번 맥주 마시면서 걸어가기.py
+++ 백준/DFS,BFS/9205번 맥주 마시면서 걸어가기.py
@@ -1,5 +1,39 @@
+# https://www.acmicpc.net/problem/9205
+
 import sys
+from collections import deque
 input=sys.stdin.readline
 
 T=int(input())
+
+
+
+def bfs(q,visited):
+    while q:
+        for i in range(1,n+2) :
+            if visited[i]==False and dis:
+
+
+
+
+for _ in range(T):
+    n=int(input())
+    home=[int(x) for x in input().split()]
+    conv=[]
+
+    for j in range(n):
+
+    visited=[False]*(n+2)
+
+    q=deque((0,0))
+    visited[0]=True
+
+
+    for _ in range(n+2):
+        x,y=map(int,input().split())
+        distance.append((x,y))
+
+    
+    bfs(q,visited)
+
 
--- 백준/DFS,BFS/연습.py
+++ 백준/DFS,BFS/연습.py
@@ -1,24 +1,5 @@
-from collections import deque
-# BFS 매서드 정의 
-def bfs(graph, start, visited):
-    # 큐 구현을 위해 deque 라이브러리 사용
-    queue=deque([start])
+home = [int(x) for x in input().split()]
 
-    # 방문한 노드 처리
-    visited[start]=True
-    while queue:
-        # 큐에서 원소 하나하나 popleft로 꺼낸다.
-        v=queue.popleft()
-        print(v, end=" ")
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-        for i in graph[v]:
-            if visited[i]==False:
-                queue.append(i)
-                visited[i]=True
+print(home)
 
-
-graph=[[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
-
-visited=[False]*9
-
-bfs(graph,1,visited)
+print(type(home[0]))
