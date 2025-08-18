2c2414f2eeb7f94dd65e4156076f4020d64e9978
jaemin <jmjung1997@gmail.com>
2025-08-18 21:34:17 +0900

    14620버 꽃길 복습

--- 백준/DFS,BFS/14620번 꽃길_dfs.py
+++ 백준/DFS,BFS/14620번 꽃길_dfs.py
@@ -1,56 +1,40 @@
-'''
-1. 꽃을 피울 수 있는 중앙 좌표의 범위는 동일하다 (1~n-1)
-2. 따라서 해당, 행과 열에 따른 탐색을 진행한다 
-- visited : 꽃을 피운 좌표를 담는 배열
-- total: 꽃을 피운 자리의 대여비용 합을 담을 변수
-
-3. 특정 좌표가 방문한적이 없다면 check함수를 통해 인접한 4개의 칸에도 방문한적이 없는지 확인한다.
-5개의 칸 모두 방문한적이 있으면 다음 좌표를 탐색하고 만약에 없으면 방문처리 및 대여비용을 누적시켜 진행한다.
-
-dfs 종료 조건은 3개의 꽃을 다 피웠을 때이다. 따라서 visited에는 꽃을 피운 좌표를 담고 있기 때문에 배열 길이가 5*3이 
-되면 dfs를 종료한다.
-또한, 시간 단축을 위해 dfs 진행 중 대여비용의 눚적이 최소 비용보다 같거나 크면 종료한다.
-'''
-
 import sys
+from itertools import combinations
+
 input=sys.stdin.readline
 
-def check(i,j,visited):
-    for idx in range(4):
-        ni=i+d[idx][0]
-        nj=j+d[idx][1]
-        if (ni,nj) in visited:
-            return False
-    return True
-
-def dfs(visited,total):
+def dfs_search(li):
     global answer
-    if total >=answer:
-        return
-    if len(visited)==3*5:
-        answer=min(answer,total)
+    visited=[] # 꽃잎이 서로 닿지 않는지 확인
+    total=0 # 꽃을 피울 때 해당 좌표의 대여 비용을 합할 변수
 
-    else:
-        for i in range(1,n-1): # 중앙점을 파악하는 코드 
-            for j in range(1,n-1):
-                if check(i,j,visited) and (i,j) not in visited:
-                    temp=[(i,j)]
-                    temp_cost=fields[i][j]
-                    for idx in range(4):
-                        ni=i+d[idx][0]
-                        nj=j+d[idx][1]
-                        temp.append((ni,nj))
-                        temp_cost+=fields[ni][nj]
+    for r,c in li:
+        visited.append((r,c)) # 꽃의 중앙 좌표를 먼저 담는다
+        total+=graph[r][c]
+        for idx in range(4):
+            nr=r+d[idx][0]
+            nc=c+d[idx][1]
 
-                    dfs(visited+temp, total+temp_cost)
+            if (nr,nc) not in visited:
+                visited.append((nr,nc))
+                total+=graph[nr][nc]
+            else: # 날개가 닿을 경우 함수를 종료한다.
+                 return
 
+    answer=min(answer,total)
+
+n=int(input())
 
 d=[(-1,0),(1,0),(0,-1),(0,1)]
 
-n=int(input())
+graph=[]
 answer=int(1e9)
-fields=[list(map(int,input().split())) for _ in range(n)]
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
 
-dfs([],0)
+candidated=[(r,c) for r in range(1,n-1) for c in range(1,n-1)]
+
+for li in combinations(candidated,3):
+    dfs_search(li)
 
 print(answer)
---
+++ 백준/그리디/21314번 민겸 수.py
@@ -0,0 +1,33 @@
+'''
+1. 최대한 잘게 나눌수록 최솟값
+2. 최대한 굵게 묶을수록 최대값
+'''
+
+
+mg=input()
+
+max_v,min_v='',''
+m=0
+for i in range(len(mg)):
+    if mg[i]=='M':
+        m+=1
+    elif mg[i]=='K':
+        if m>0:
+            max_v+=str(5*(10**m))
+            min_v+=str(10**m+5)
+        
+        else:
+            max_v+='5'
+            min_v+='5'
+        
+        m=0
+
+# 끝부분이 M으로 끝나는 경우
+if m>0:
+    for i in range(m):
+        max_v+='1'
+    min_v+=str(10**(m-1))
+
+print(max_v)
+print(min_v)
+
